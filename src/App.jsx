9b0699ed4493e6fa1d14f8e54429a3da7fd7df82
Nellie Allsop <nellie.allsop@gmail.com>
2023-10-26 14:55:15 +0100

    submission for lab

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Footer from "./components/Footer";
 import data from "./Data"
 import {useState} from "react"
 import SelectedBeast from "./components/SelectedBeast"
-import Form
 
 function App() {
 	const [showBeast, setShowBeast] = useState(false)
@@ -13,15 +12,37 @@ function App() {
 	const handleClose = () => {
 		setShowBeast(false)
 	}
+	const [form, setForm]=useState({
+		numHorns: "",
+	})
 
 	function handleShowBeast(beast){
 		setShowBeast(!showBeast);
 		setShownBeast(beast)
 	}
 
+	function handleSubmit(event) {
+		event.preventDefault();
+	}
+
+	function handleChange(event) {
+		setForm({ ...form, [event.target.name]: event.target.value})
+	}
+
 	return (
 		<div>
 			<Header />
+			<form onsubmit = {handleSubmit}>
+<label> Filter by number of horns </label>
+<input name="numHorns" list="numHorns" onChange={handleChange}/>
+<datalist id="numHorns">
+	<option value="1"></option>
+	<option value="2"></option>
+	<option value="3"></option>
+	<option value="100"></option>
+</datalist>
+<button>Submit</button>
+</form>
 			<Gallery data={data} handleShowSelectedBeast={handleShowBeast} />
 			<Footer />
 			{showBeast && (<SelectedBeast onClose={handleClose} shownBeast={shownBeast}
